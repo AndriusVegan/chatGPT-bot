a4ffbaf5fa3edf6a42ed4b17a878cf32fceb8a25
AndriusVegan <lifecoachandy7@gmail.com>
2023-02-21 18:14:57 +0200

    chat and chatInput mostly 🐲

---
+++ components/Chat.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  chatId: string;
+};
+
+function Chat({ chatId }: Props) {
+  return <div className="flex">Chat</div>;
+}
+
+export default Chat;
--- firebase.ts
+++ firebase.ts
@@ -1,10 +1,5 @@
-import { getAop, getApps, initializeApp } from "firebase/app";
-import { getFireStore } from "firebases/firestore";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,4 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
